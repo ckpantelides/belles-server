86f9b5403256fcd9ef24c0053b21f002d065d5db
CK Pantelides <ckpantelides@gmail.com>
2019-02-18 19:54:19 +0000

    environment variables

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 var cors = require("cors");
 
+// Stripe test & live secret keys
+const testKey = process.env.testkey;
+const liveKey = process.env.livekey;
+
+var stripe = require("stripe")(testKey);
+
 const app = express();
 
 app.use(function(req, res, next) {
@@ -20,13 +26,36 @@ app.use(bodyParser.json());
 
 app.post("/post", function(req, res) {
   const newOrder = req.body;
-  console.log(newOrder.body.token.id);
-  console.log(newOrder.body.args.billing_name);
-});
+  // console.log(newOrder.body.token.id);
+  // console.log(newOrder.body.args.billing_name);
+  // console.log(newOrder);
 
-const HTTP_PORT = 4000;
+  // email can be added for stripe auto receipts:
+  // receipt_email: "ckp83@hotmail.com"
+
+  // Token is created using Checkout or Elements!
+  // Get the payment token ID submitted by the form:
+  const token = newOrder.body.token.id;
+
+  (async () => {
+    const charge = await stripe.charges.create({
+      amount: newOrder.body.price,
+      currency: "gbp",
+      description: "Example charge",
+      source: token
+    });
+  })();
+});
+/*
+const HTTP_PORT = 5400;
 
 app.listen(HTTP_PORT);
 console.log(`Server running on port ${HTTP_PORT}`);
+*/
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${PORT}`);
+});
 
 module.exports = app;
